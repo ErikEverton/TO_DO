67b28ad05f2c133668bf20be4c9ce08a88d6bbde
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-01-28 10:54:34 -0300

    Todo model modification

--- mysite/TO_DO/models.py
+++ mysite/TO_DO/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateTimeField("date published")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
